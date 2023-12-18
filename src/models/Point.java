fbbab78b4daff2cf7306e9131ea732358c6774c0
TarshSeriy <tarsergey5@gmail.com>
2023-12-18 08:43:48 +0500

    Something

---
+++ src/models/Point.java
@@ -0,0 +1,19 @@
+package models;
+import java.lang.reflect.Array;
+
+
+public class Point {
+    private double x;
+    private double y;
+
+    public Point(double x, double y){
+        this.x=x;
+        this.y=y;
+
+    }
+    public void SetPointX(double x){
+        this.x=x;
+    }
+
+
+}
---
+++ src/models/Shape.java
@@ -0,0 +1,21 @@
+package models;
+import java.util.ArrayList;
+public class Shape {
+    private ArrayList<Point> points;
+
+    public Shape(){
+        points= new ArrayList<>();
+
+    }
+    public void addPoint(Point point){
+        points.add(point);
+
+    }
+    public double calculatorPerimetr{
+
+    }
+
+}
+
+        
+
